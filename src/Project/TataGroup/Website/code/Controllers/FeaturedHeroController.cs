bd8e59a33251b2020c3140002eaff25a9b4c46d8
Administrator <Administrator@DB068C3488B0500>
2025-04-25 17:08:02 +0530

    Featured Hero Controller

---
+++ src/Project/TataGroup/Website/code/Controllers/FeaturedHeroController.cs
@@ -0,0 +1,28 @@
+﻿using Sitecore.Mvc.Presentation;
+using Sitecore.Web.UI.WebControls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Tata.Project.TataGroupWeb.Models;
+
+namespace Tata.Project.TataGroupWeb.Controllers
+{
+    public class FeaturedHeroController : Controller
+    {
+        // GET: FeaturedHero
+        public ActionResult Index()
+        {
+            var renderingItem = RenderingContext.Current.ContextItem;
+            HeroPage heroPage = new HeroPage()
+            {
+                HeroIntro = new HtmlString(FieldRenderer.Render(renderingItem, "PageIntro")),
+                HeroDescription = new HtmlString(FieldRenderer.Render(renderingItem, "PageDescription")),
+                HeroBackgroundImage = new HtmlString(FieldRenderer.Render(renderingItem, "PageBackgroundImage"))
+            };
+
+            return View("~/Views/Tata/HeroPage/Index.cshtml", heroPage);
+        }
+    }
+}
